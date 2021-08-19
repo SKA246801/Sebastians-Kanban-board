373c7ea89bc01c84687ec5cbe798564126e38a9b
Sebastian <sebastianalvarado246801@gmail.com>
2021-08-18 19:54:26 -0500

    created server

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return (
+   <section>
+     <Dashboard></Dashboard>
+   </section>
+  );
+}
+
+export default App;
