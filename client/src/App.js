fc8c8816101ae629e8aa9f3fa28a7cd48a7f5660
Sadrac Tijerina <reavity3@gmail.com>
2021-08-20 21:38:22 -0500

    added list component with title coponent

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import '@atlaskit/css-reset'
-import { DragDropContext } from 'react-beautiful-dnd'
-import Dashboard from './components/Dashboard'
-import KanbanBoard from './components/KanbanBoard'
-import Column from './components/Column'
+import List  from './components/List/List'
+
 
 function App() {
   return (
-    <KanbanBoard/>
+    <div>
+      <List/>
+    </div>
   )
 }
 
--- client/src/components/Dashboard/index.js
+++
@@ -1,9 +0,0 @@
-import react from 'react'
-
-function Dashboard() {
-    return (
-        <div>Dashboard fdasfdsfsa</div>
-    )
-}
-
-export default Dashboard
