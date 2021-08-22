a9db645370254c578350fde8cf6544719a842a93
Sadrac Tijerina <reavity3@gmail.com>
2021-08-22 10:08:29 -0500

    restructured code

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
-import './App.css'
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Dashboard from './components/Dashboard'
-import Navbar from './components/Navbar'
-import Account from './components/Account'
-import Support from './components/Support'
-import Messaging from './components/Messaging'
-import Kanban from './components/Kanban'
-
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
+import Account from "./components/Account";
+import Support from "./components/Support";
+import Messaging from "./components/Messaging";
+import Kanban from "./components/Kanban";
 
 function App() {
   return (
-   <>
-     <Router>
+    <>
+      <Router>
         <Navbar />
         <Switch>
-            <Route path='/dashboard' component={Dashboard}/>
-            <Route path='/account' component={Account}/>
-            <Route path='/messaging' component={Messaging}/>
-            <Route path='/support' component={Support}/>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/account" component={Account} />
+          <Route path="/messaging" component={Messaging} />
+          <Route path="/support" component={Support} />
         </Switch>
-     </Router>
-   </>
+      </Router>
+    </>
   );
 }
 
