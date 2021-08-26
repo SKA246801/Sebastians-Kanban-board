51163837d00a29a437ada04c8619b383c30774f1
SKA246801 <80431875+SKA246801@users.noreply.github.com>
2021-08-26 15:18:12 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const path = require('path')
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-const express = require("express");
 
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
